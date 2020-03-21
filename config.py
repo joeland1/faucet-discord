7123e518278bcf1ee8e40cf9ecc605112ecc7dab
joeland1 <55666119+joeland1@users.noreply.github.com>
2020-03-20 21:32:40 -0400

    asdasd
    asdgasdga

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 rpc_user = 'user'
 rpc_password ='password'
 ip = '127.0.0.1'
-rpc_port = '34229'
+rpc_port = '51473'
 
-AMOUNT=0.5
-TIME = 3600 #amount of seconds
-VERSION = "1.0.1"
-FAUCET_SOURCE = "General"
-TOKEN = ''
+AMOUNT=0.1
+TIME = 86400 #amount of seconds
+VERSION = "1.0.0"
+FAUCET_SOURCE = "Faucet"
+TOKEN = 'token'
 CAPTCHALENGTH = 5
-OWNER_ID = 259837465299255296
-PREFIX = '//'
+OWNER_ID = 665413544837120041 #make int
+PREFIX = '!DOGEC '
